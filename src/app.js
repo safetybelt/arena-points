5479a40a0d124e56222be74c0f1b379b62445cc3
Jonathan Neely <jneely@gmail.com>
2022-11-21 12:03:04 -0800

    Updated text for WotLK Classic; also added Survey for incorrect calculations

--- src/app.js
+++ src/app.js
@@ -7,7 +7,10 @@ function App() {
 
     return (
         <div className="app">
-            <h1>TBC Season 4 Arena Points Calculator</h1>
+            <h1>
+                Wrath of the Lich King Classic:<br />
+                Arena Points Calculator
+            </h1>
             <nav>
                 <ul>
                     <li>
@@ -24,10 +27,15 @@ function App() {
             </nav>
             {calculator === 'rtp' ? <RatingToPoints /> : <PointsToRating />}
             <aside>
-                <em>
-                    Special thanks to <a href="https://www.reddit.com/user/Finalbelle">Finalbelle</a> on Reddit for the calculations.<br/>
-                    <a href="https://www.reddit.com/r/classicwow/comments/us1cg5/tbc_arena_calculator_formula_updated_for_s4/">Original post can be found here</a>.
-                </em>
+                <p>
+                    If you received a different number of points than expected, please fill out <a href="https://forms.gle/yhgaF9uUvdsuFKhQ9">this form</a> to help us fix any inconsistencies in the calculator.
+                </p>
+                <p>
+                    <em>
+                        Special thanks to <a href="https://www.reddit.com/user/Finalbelle">Finalbelle</a> on Reddit for the calculations.<br/>
+                        <a href="https://www.reddit.com/r/classicwow/comments/us1cg5/tbc_arena_calculator_formula_updated_for_s4/">Original post can be found here</a>.
+                    </em>
+                </p>
             </aside>
             <div className="buy-beer-wrapper">
                 <a href="http://buymeacoffee.com/safetybelt" className="as-btn">🍺 Buy me a beer</a>
